f80720002805c871227f5019479f429cd899d7a7
gioshaverdovi <45195145+gioshaverdovi@users.noreply.github.com>
2019-05-11 21:05:18 +0400

    add

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 var typing = document.getElementById('type');
 var post = document.getElementById('posted');
 var share = document.getElementById('shared');
-// var close = document.getElementById('close');
 
 
 typing.focus();
 
 function addRemove() {
     var element = document.createElement('div');
+    element.style.display = "flex";
+    element.style.justifyContent = "space-between";
     element.innerText = typing.value;
     post.appendChild(element);
     typing.value = '';
     typing.focus();
-    var close = document.createElement('span');
-    close.innerText = " X";
-    close.classList.add('close');
+
+
+    var close = document.createElement('button');
+    close.innerText = "delete"
+    close.type="button";
     element.appendChild(close);
     close.addEventListener('click', function(event){
         event.target.parentNode.remove();
-    })
+    });
 }
 
 typing.addEventListener('keyup', function(event){
-    if(event.which == 13){
+    if(event.which == 13 && typing.value != ''){
         addRemove();
+    }
+    else if (event.which == 13 && typing.value == ''){
+        window.alert('შეიყვანეთ ტექსტი');
     }
 });
 
 share.addEventListener('click', function(){
-    addRemove();
+    if (typing.value != ''){
+        addRemove();
+    }
+    else {
+        window.alert('შეიყვანეთ ტექსტი');
+    }
 });
