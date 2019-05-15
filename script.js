771f41be992eb94930785a8a0823593e0efe75ba
gioshaverdovi <giorgi.shaverdovi11b@geolab.edu.ge>
2019-05-15 16:07:32 +0400

    add

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
 var typing = document.getElementById('type');
 var post = document.getElementById('posted');
 var share = document.getElementById('shared');
-
+var drop = document.getElementById('drope');
 
 typing.focus();
-let output = `<div class="dropdown">
-<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-  
-</button>
-<div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-  <button class="dropdown-item" type="button">Action</button>
-  <button class="dropdown-item" type="button">Another action</button>
-  <button class="dropdown-item" type="button">Something else here</button>
-</div>
-</div>`;
 
 function addRemove() {
     var element = document.createElement('div');
-    element.style.display = "flex";
-    element.style.justifyContent = "space-between";
     element.innerText = typing.value;
     post.appendChild(element);
     typing.value = '';
     typing.focus();
-
-
-    var close = document.createElement('button');
-    close.innerText = "delete"
-    close.type="button";
-    element.appendChild(close);
-    close.addEventListener('click', function(event){
-        event.target.parentNode.remove();
-    });
 }
 
 typing.addEventListener('keyup', function(event){
